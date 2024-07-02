b545aa890ca3f51aea8f2b5cfe1d7ae425b07a9f
Hesham Elnemr <hesham.elnemr@gmail.com>
2024-07-02 13:51:54 +0300

    fix decoder

--- lib/src/core/decode_image.dart
+++ lib/src/core/decode_image.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: deprecated_member_use
 
-import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
+import 'dart:ui';
 
+import 'package:flutter/material.dart';
 import 'package:photo_manager/photo_manager.dart';
 
 class DecodeImage extends ImageProvider<DecodeImage> {
@@ -19,14 +20,15 @@ class DecodeImage extends ImageProvider<DecodeImage> {
   });
 
   @override
-  ImageStreamCompleter load(DecodeImage key, DecoderCallback decode) {
+  ImageStreamCompleter loadImage(DecodeImage key, ImageDecoderCallback decode) {
     return MultiFrameImageStreamCompleter(
       codec: _loadAsync(key, decode),
       scale: key.scale,
     );
   }
 
-  Future<ui.Codec> _loadAsync(DecodeImage key, DecoderCallback decode) async {
+  Future<ui.Codec> _loadAsync(
+      DecodeImage key, ImageDecoderCallback decode) async {
     assert(key == this);
 
     final coverEntity =
@@ -35,7 +37,7 @@ class DecodeImage extends ImageProvider<DecodeImage> {
     final bytes = await coverEntity
         .thumbnailDataWithSize(ThumbnailSize(thumbSize, thumbSize));
 
-    return decode(bytes!);
+    return decode(await ImmutableBuffer.fromUint8List(bytes!));
   }
 
   @override
